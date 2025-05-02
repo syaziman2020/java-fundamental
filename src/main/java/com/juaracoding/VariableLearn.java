c3373ad0bf524ccd7c579ebd0d946d4f307828f8
*syaziman2020* <syazimanuray@gmail.com>
2025-05-02 21:47:05 +0700

    learn about varible and data type

---
+++ src/main/java/com/juaracoding/VariableLearn.java
@@ -0,0 +1,23 @@
+package com.juaracoding;
+
+public class VariableLearn {
+
+    public static void main(String[] args) {
+        String name, phone;
+
+        short age = 20;
+
+        name  = "Uray Syaziman";
+
+        phone = "0877454543";
+
+        char grade = 'A';
+
+        float rating= 4.7f;
+
+        boolean isVerify = false;
+
+        System.out.println("nama: "+name+"\n"+"usia: "+age+"\n"+ "Telepon: "+phone+"\n"+"grade: "+grade+"\n"+"status: "+isVerify+"\n"+"rating: "+rating);
+
+    }
+}
